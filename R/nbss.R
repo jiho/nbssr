838ddfe8829829aed2a0023d63798d377e5090b1
Jean-Olivier Irisson <irisson@normalesup.org>
2020-05-15 01:40:21 +0200

    Add weights

--- R/nbss.R
+++ R/nbss.R
@@ -1,6 +1,7 @@
 #' Normalised Biomass Size Spectrum
 #'
 #' @param x vector of biomasses, biovolumes, or lengths.
+#' @param w vector of weights, typically concentrations associated with individual measurements in `x`.
 #' @param type whether to compute a biomass/biovolume or abundance spectrum.
 #' @param base base of the logarithm for the computation of bins.
 #' @param binwidth width of bins in log10 scale.
@@ -25,7 +26,7 @@
 #' # Abundance spectrum
 #' ss <- nbss(uvp$length_mm, binwidth=0.05)
 #' autoplot(ss) + labs(x="Length (mm)")
-nbss <- function(x, type=c("biomass", "abundance"), base=10, binwidth=0.1) {
+nbss <- function(x, w=rep(1, length(x)), type=c("biomass", "abundance"), base=10, binwidth=0.1) {
 
   # check arguments
   type <- match.arg(type)
@@ -39,6 +40,9 @@ nbss <- function(x, type=c("biomass", "abundance"), base=10, binwidth=0.1) {
     l <- log10
     il <- function(x) {10^x}
   }
+  if (length(x) != length(w)) {
+    stop("x and w must be the same length")
+  }
 
   # log transform input data
   x_log <- l(x)
@@ -48,11 +52,11 @@ nbss <- function(x, type=c("biomass", "abundance"), base=10, binwidth=0.1) {
 
   # compute biomass/abundance in each bin
   if (type == "biomass") {
-    fun <- sum
+    xx <- x * w
   } else {
-    fun <- length
+    xx <- w
   }
-  ss <- aggregate(x, list(x_log_bin), FUN=fun)
+  ss <- aggregate(xx, list(x_log_bin), FUN=sum)
   names(ss) <- c("bin_log", "y")
 
   # compute middle of bin in the original scale
